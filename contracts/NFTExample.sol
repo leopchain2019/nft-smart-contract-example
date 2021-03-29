cca22a5d9dc80dfc8733be4935b0a1bb03a9796b
Sebastian Stoll <4408878+stoll@users.noreply.github.com>
2021-03-30 00:57:21 +0200

    Update NFTExample.sol

--- contracts/NFTExample.sol
+++ contracts/NFTExample.sol
@@ -2,17 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTExample is ERC721, Ownable {
+contract NFTExample is ERC721URIStorage, Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor()
         ERC721("NFT Name", "SYMBOL")
-        public
     {
         //
     }
